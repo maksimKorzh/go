e93d67dd36f2d89d8c1216b1b08ef8ea8561e66f
Code Monkey King <freesoft.for.people@gmail.com>
2024-10-01 15:21:24 +0300

    debugging 6 block network

---
+++ js/goban.js
@@ -0,0 +1,366 @@
+/*****************************************\
+  =======================================
+ 
+                 Gobang JS
+
+                    by
+
+             Code Monkey King
+
+  =======================================
+\*****************************************/
+
+const Goban = function(params) {
+  // CANVAS
+  var canvas, ctx, cell;
+
+  // DATA
+  const EMPTY = 0
+  const BLACK = 1
+  const WHITE = 2
+  const MARKER = 4
+  const OFFBOARD = 7
+  const LIBERTY = 8
+
+  var board = [];
+  var history = [];
+  var komi = 6.5;
+  var size;
+  var side = BLACK;
+  var liberties = [];
+  var block = [];
+  var ko = EMPTY;
+  var bestMove = EMPTY;
+  var userMove = 0;
+  var moveCount = 0;
+
+  // PRIVATE METHODS
+  function drawBoard() { /* Render board to screen */
+    cell = canvas.width / size;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    for (let i = 1; i < size-1; i++) {
+      const x = i * cell + cell / 2;
+      const y = i * cell + cell / 2;
+      let offset = cell * 2 - cell / 2;
+      ctx.moveTo(offset, y);
+      ctx.lineTo(canvas.width - offset, y);
+      ctx.moveTo(x, offset);
+      ctx.lineTo(x, canvas.height - offset);
+    };ctx.stroke();
+    for (let row = 0; row < size; row++) {
+      for (let col = 0; col < size; col++) {
+        let sq = row * size + col;
+        let starPoints = {
+           9: [36, 38, 40, 58, 60, 62, 80, 82, 84],
+          13: [64, 67, 70, 109, 112, 115, 154, 157, 160],
+          19: [88, 94, 100, 214, 220, 226, 340, 346, 352]
+        }
+        if ([9, 13, 19].includes(size-2) && starPoints[size-2].includes(sq)) {
+          ctx.beginPath();
+          ctx.arc(col * cell+(cell/4)*2, row * cell +(cell/4)*2, cell / 6 - 2, 0, 2 * Math.PI);
+          ctx.fillStyle = 'black';
+          ctx.fill();
+          ctx.stroke();
+        }
+        if (board[sq] == 7) continue;
+        let color = board[sq] == 1 ? "black" : "white";
+        if (board[sq]) {
+          ctx.beginPath();
+          ctx.arc(col * cell + cell / 2, row * cell + cell / 2, cell / 2 - 2, 0, 2 * Math.PI);
+          ctx.fillStyle = color;
+          ctx.fill();
+          ctx.stroke();
+        }
+        if (sq == userMove) {
+          let color = board[sq] == 1 ? "white" : "black";
+          ctx.beginPath();
+          ctx.arc(col * cell+(cell/4)*2, row * cell +(cell/4)*2, cell / 5 - 2, 0, 2 * Math.PI);
+          ctx.fillStyle = color;
+          ctx.fill();
+          ctx.stroke();
+        }
+      }
+    }
+  }
+
+  function userInput(event) { /* Handle user input */
+    if (params.sgf) return;
+    let rect = canvas.getBoundingClientRect();
+    let mouseX = event.clientX - rect.left;
+    let mouseY = event.clientY - rect.top;
+    let col = Math.floor(mouseX / cell);
+    let row = Math.floor(mouseY / cell);
+    let sq = row * size + col;
+    if (board[sq]) return;
+    if (!setStone(sq, side, true)) return;
+    drawBoard();
+    setTimeout(function() { try { params.response(); } catch (e) { /* Specify custom response function */ } }, 100)
+  }
+
+  function clearBoard() { /* Empty board, set offboard squares */
+    for (let sq = 0; sq < size ** 2; sq++) {
+      switch (true) {
+        case (sq < size):
+        case (sq >= (size ** 2 - size)):
+        case (!(sq % size)):
+          board[sq] = OFFBOARD;
+          board[sq-1] = OFFBOARD;
+          break;
+        default: board[sq] = 0;
+      }
+    }
+  }
+
+  function setStone(sq, color, user) { /* Place stone on board */
+    if (board[sq] != EMPTY) {
+      if (user) alert("Illegal move!");
+      return false;
+    } else if (sq == ko) {
+      if (user) alert("Ko!");
+      return false;
+    } let old_ko = ko;
+    ko = EMPTY;
+    board[sq] = color;
+    captures(3 - color, sq);
+    count(sq, color);
+    let suicide = liberties.length ? false : true; 
+    restoreBoard();
+    if (suicide) {
+      board[sq] = EMPTY;
+      ko = old_ko;
+      moveCount--;
+      if (user) alert("Suicide move!");
+      return false;
+    } 
+    side = 3 - side;
+    userMove = sq;
+    history.push({
+      'ply': moveCount+1,
+      'side': (3-color),
+      'move': sq,
+      'board': JSON.stringify(board),
+      'ko': ko
+    });
+    moveCount = history.length-1;
+    return true;
+  }
+
+  function setHandicap(stones) {
+    if (stones < 0 || stones > 9) return;
+    let handicap = stones;
+    let handicapStones = [88, 100, 340, 352, 214, 226, 94, 346, 220].slice(0, handicap);
+    for (let sq of handicapStones) {
+      goban.play(sq, goban.BLACK);
+      if (sq != handicapStones[handicap-1]) goban.pass();
+      goban.refresh();
+    }
+  }
+
+  function pass() {
+    history.push({
+      'ply': moveCount+1,
+      'side': (3-side),
+      'move': EMPTY,
+      'board': JSON.stringify(board),
+      'ko': ko
+    });
+    moveCount = history.length-1;
+    ko = EMPTY;
+    side = 3 - side;
+  }
+
+  function count(sq, color) { /* Count group liberties */
+    stone = board[sq];
+    if (stone == OFFBOARD) return;
+    if (stone && (stone & color) && (stone & MARKER) == 0) {
+      block.push(sq);
+      board[sq] |= MARKER;
+      for (let offset of [1, size, -1, -size]) count(sq+offset, color);
+    } else if (stone == EMPTY) {
+      board[sq] |= LIBERTY;
+      liberties.push(sq);
+    }
+  }
+
+  function restoreBoard() { /* Remove group markers */
+    block = []; liberties = []; points_side = [];
+    for (let sq = 0; sq < size ** 2; sq++) {
+      if (board[sq] != OFFBOARD) board[sq] &= 3;
+    }
+  }
+
+  function captures(color, move) { /* Handle captured stones */
+    for (let sq = 0; sq < size ** 2; sq++) {
+      let stone = board[sq];
+      if (stone == OFFBOARD) continue;
+      if (stone & color) {
+        count(sq, color);
+        if (liberties.length == 0) clearBlock(move);
+        restoreBoard()
+      }
+    }
+  }
+
+  function clearBlock(move) { /* Erase stones when captured */
+    if (block.length == 1 && inEye(move, 0) == 3-side) ko = block[0];
+    for (let i = 0; i < block.length; i++)
+      board[block[i]] = EMPTY;
+  }
+
+  function inEye(sq) { /* Check if sqaure is in diamond shape */
+    let eyeColor = -1;
+    let otherColor = -1;
+    for (let offset of [1, size, -1, -size]) {
+      if (board[sq+offset] == OFFBOARD) continue;
+      if (board[sq+offset] == EMPTY) return 0;
+      if (eyeColor == -1) {
+        eyeColor = board[sq+offset];
+        otherColor = 3-eyeColor;
+      } else if (board[sq+offset] == otherColor)
+        return 0;
+    }
+    if (eyeColor > 2) eyeColor -= MARKER;
+    return eyeColor;
+  }
+
+  function loadHistoryMove() {
+    let move = history[moveCount];
+    board = JSON.parse(move.board);
+    side = move.side;
+    ko = move.ko;
+    userMove = move.move;
+    drawBoard();
+  }
+
+  function undoMove() {
+    if (moveCount == 0) return;
+    moveCount--;
+    history.pop();
+    loadHistoryMove();
+  }
+
+  function firstMove() {
+    moveCount = 0;
+    loadHistoryMove();
+  }
+
+  function prevMove() {
+    if (moveCount == 0) return;
+    moveCount--;
+    loadHistoryMove();
+  }
+
+  function prevFewMoves(few) {
+    if (moveCount == 0) return;
+    if ((moveCount - few) >= 0) moveCount -= few;
+    else firstMove();
+    loadHistoryMove();
+  }
+
+  function nextMove() {
+    if (moveCount == history.length-1) return;
+    moveCount++;
+    loadHistoryMove();
+  }
+
+  function nextFewMoves(few) {
+    if (moveCount == history.length-1) return;
+    if ((moveCount + few) <= history.length-1) moveCount += few;
+    else lastMove();
+    loadHistoryMove();
+  }
+
+  function lastMove() {
+    moveCount = history.length-1
+    loadHistoryMove();
+  }
+
+  function loadSgf(sgf) {
+    for (let move of sgf.split(';')) {
+      if (move.length) {
+        if (move.charCodeAt(2) < 97 || move.charCodeAt(2) > 115) { continue; }
+        let player = move[0] == 'B' ? BLACK : WHITE;
+        let col = move.charCodeAt(2)-97;
+        let row = move.charCodeAt(3)-97;
+        let sq = (row+1) * 21 + (col+1);
+        setStone(sq, player, false);
+      }
+    } firstMove();
+  }
+
+  function saveSgf() {
+    let sgf = '(';
+    for (let item of history.slice(1, history.length)) {
+      let col = item.move % 21;
+      let row = Math.floor(item.move / 21);
+      let color = item.side == BLACK ? 'W' : 'B';
+      let coords = ' abcdefghijklmnopqrs';
+      let move = coords[col] + coords[row];
+      if (move == '  ') sgf += ';' + color + '[]'
+      else sgf += ';' + color + '[' + move + ']';
+    } sgf += ')'
+    return sgf;
+  }
+
+  function resizeCanvas() {
+    let offset = params.offset == undefined ? 0 : params.offset;
+    if (window.innerWidth > window.innerHeight) {
+      canvas.width = window.innerHeight - offset; // Desktop
+      canvas.height = canvas.width;
+    } else {
+      canvas.width = window.innerWidth - offset;  // Mobile
+      canvas.height = canvas.width;
+    } drawBoard();
+  }
+
+  function init() { /* Init goban module */
+    let container = document.getElementById('goban');
+    canvas = document.createElement('canvas');
+    canvas.style="margin-bottom: -3%;";
+    container.appendChild(canvas);
+    size = params.size+2;
+    canvas.addEventListener('click', userInput);
+    ctx = canvas.getContext('2d');
+    clearBoard();
+    history.push({
+      'ply': 0,
+      'side': BLACK,
+      'move': EMPTY,
+      'board': JSON.stringify(board),
+      'ko': ko
+    });
+    moveCount = history.length-1;
+    window.addEventListener('resize', resizeCanvas);
+    resizeCanvas();
+  }
+  
+  // PUBLIC API
+  return {
+    init: init(),
+    BLACK: BLACK,
+    WHITE: WHITE,
+    importSgf: function(sgf) { return loadSgf(sgf); },
+    exportSgf: function() { return saveSgf(); },
+    position: function() { return board; },
+    setKomi: function(komiVal) { komi = komiVal; },
+    komi: function() { return komi; },
+    history: function() { return history; },
+    side: function() { return side; },
+    ko: function() { return ko; },
+    count: function(sq, color) { return count(sq, color); },
+    liberties: function() { return liberties; },
+    restore: function() { return restoreBoard(); },
+    play: function(sq, color, user) { return setStone(sq, color, user); },
+    handicap: function(stones) { return setHandicap(stones); },
+    pass: function() { return pass(); },
+    refresh: function() { return drawBoard(); },
+    undoMove: function() { return undoMove(); },
+    firstMove: function() { return firstMove(); },
+    prevFewMoves: function(few) { return prevFewMoves(few); },
+    prevMove: function() { return prevMove(); },
+    nextMove: function() { return nextMove(); },
+    nextFewMoves: function(few) { return nextFewMoves(few); },
+    lastMove: function() { return lastMove(); }
+  }
+}
---
+++ js/model.js
@@ -0,0 +1,165 @@
+// DATA
+const batches = 1;
+const inputBufferLength = 19 * 19;
+const inputBufferChannels = 22;
+const inputGlobalBufferChannels = 19;
+const global_inputs = new Float32Array(batches * inputGlobalBufferChannels);
+var computerSide = 2;
+var level = 'dan';
+
+// PARSE PARAMS
+/*const urlParams = new URLSearchParams(window.location.search);
+try {
+  setTimeout(function() {
+    if (urlParams.get('komi')) goban.setKomi(parseFloat(urlParams.get('komi')));
+    goban.handicap(parseInt(urlParams.get('handicap')));
+    level = urlParams.get('level');
+  }, 100);
+} catch (e) { console.log(e); }
+*/
+
+
+// QUERY MODEL
+function inputTensor() { /* Convert GUI goban.position() to katago model input tensor */
+  let katago = computerSide;
+  let player = (3-computerSide);
+  if (level == 'kyu') {
+    player = computerSide;
+    katago = (3-computerSide);
+  }
+  const bin_inputs = new Float32Array(batches * inputBufferLength * inputBufferChannels);
+  for (let y = 0; y < 19; y++) {
+    for (let x = 0; x < 19; x++) {
+      sq_19x19 = (19 * y + x);
+      sq_21x21 = (21 * (y+1) + (x+1))
+      bin_inputs[inputBufferChannels * sq_19x19 + 0] = 1.0;
+      if (goban.position()[sq_21x21] == katago) bin_inputs[inputBufferChannels * sq_19x19 + 1] = 1.0;
+      if (goban.position()[sq_21x21] == player) bin_inputs[inputBufferChannels * sq_19x19 + 2] = 1.0;
+      if (goban.position()[sq_21x21] == katago || goban.position()[sq_21x21] == player) {
+        let libs_black = 0;
+        let libs_white = 0;
+        goban.count(sq_21x21, goban.BLACK);
+        libs_black = goban.liberties().length;
+        goban.restore();
+        goban.count(sq_21x21, goban.WHITE);
+        libs_white = goban.liberties().length;
+        goban.restore();
+        if (libs_black == 1 || libs_white == 1) bin_inputs[inputBufferChannels * sq_19x19 + 3] = 1.0;
+        if (libs_black == 2 || libs_white == 2) bin_inputs[inputBufferChannels * sq_19x19 + 4] = 1.0;
+        if (libs_black == 3 || libs_white == 3) bin_inputs[inputBufferChannels * sq_19x19 + 5] = 1.0;
+      }
+    }
+  }
+  if (sq_19x19 == goban.ko()) bin_inputs[inputBufferChannels * sq_19x19 + 6] = 1.0;
+  let moveIndex = goban.history().length-1;
+  if (moveIndex >= 1 && goban.history()[moveIndex-1].side == player) {
+    let prevLoc1 = goban.history()[moveIndex-1].move;
+    let x = prevLoc1 % 21;
+    let y = Math.floor(prevLoc1 / 21);
+    if (prevLoc1) bin_inputs[inputBufferChannels * (19 * y + x) + 9] = 1.0;
+    else global_inputs[0] = 1.0;
+    if (moveIndex >= 2 && goban.history()[moveIndex-2].side == katago) {
+      let prevLoc2 = goban.history()[moveIndex-2].move;
+      let x = prevLoc2 % 21;
+      let y = Math.floor(prevLoc2 / 21);
+      if (prevLoc2) bin_inputs[inputBufferChannels * (19 * y + x) + 10] = 1.0;
+      else global_inputs[1] = 1.0;
+      if (moveIndex >= 3 && goban.history()[moveIndex-3].side == player) {
+        let prevLoc3 = goban.history()[moveIndex-3].move;
+        let x = prevLoc3 % 21;
+        let y = Math.floor(prevLoc3 / 21);
+        if (prevLoc3) bin_inputs[inputBufferChannels * (19 * y + x) + 11] = 1.0;
+        else global_inputs[2] = 1.0;
+        if (moveIndex >= 4 && goban.history()[moveIndex-4].side == katago) {
+          let prevLoc4 = goban.history()[moveIndex-4].move;
+          let x = prevLoc4 % 21;
+          let y = Math.floor(prevLoc4 / 21);
+          if (prevLoc4) bin_inputs[inputBufferChannels * (19 * y + x) + 12] = 1.0;
+          else global_inputs[3] = 1.0;
+          if (moveIndex >= 5 && goban.history()[moveIndex-5].side == player) {
+            let prevLoc5 = goban.history()[moveIndex-5].move;
+            let x = prevLoc5 % 21;
+            let y = Math.floor(prevLoc5 / 21);
+            if (prevLoc5) bin_inputs[inputBufferChannels * (19 * y + x) + 13] = 1.0;
+            else global_inputs[4] = 1.0;
+          }
+        }
+      }
+    }
+  }
+  let selfKomi = (computerSide == goban.WHITE ? goban.komi()+1 : -goban.komi());
+  global_inputs[5] = selfKomi / 20.0
+  return bin_inputs;
+}
+
+async function play() { /* Play best move */
+  if (editMode) return;
+  document.getElementById('stats').innerHTML = 'KataNet is thinking...';
+  computerSide = goban.side();
+  const bin_inputs = inputTensor();
+  try {
+    tf.setBackend("cpu");
+    const model = await tf.loadGraphModel("./model/model.json");
+    const results = await model.executeAsync({
+        "swa_model/bin_inputs": tf.tensor(bin_inputs, [batches, inputBufferLength, inputBufferChannels], 'float32'),
+        "swa_model/global_inputs": tf.tensor(global_inputs, [batches, inputGlobalBufferChannels], 'float32')
+    });
+
+    const policyTensor = results[3];
+    const policyArray = await policyTensor.slice([0, 0, 0], [1, 1, 361]).array();
+    const flatPolicyArray = policyArray[0][0];
+    let scores = results[1];
+    let flatScores = scores.dataSync();
+    let copyPolicy = JSON.parse(JSON.stringify(flatPolicyArray));
+    let topPolicies = copyPolicy.sort((a, b) => b - a).slice(0, 5);
+    let topMoves = [];
+    for (let move of topPolicies) topMoves.push(flatPolicyArray.indexOf(move));
+    for (let move = 0; move < topMoves.length; move++) {
+      if (goban.history().length < 10) move = Math.floor(Math.random() * 3);
+      let best_19 = topMoves[0];//topMoves[move];
+      let row_19 = Math.floor(best_19 / 19);
+      let col_19 = best_19 % 19;
+      let scoreLead = (flatScores[2]*20).toFixed(2);
+      console.log(flatScores, scoreLead);
+      let katagoColor = computerSide == goban.BLACK ? 'Black' : 'White';
+      let playerColor = (3-computerSide) == goban.BLACK ? 'Black' : 'White';
+      /*if (level == 'kyu') {
+        scoreLead -= (20+goban.komi());
+        scoreLead = scoreLead.toFixed(2);
+        katagoColor = (3-computerSide) == goban.BLACK ? 'Black' : 'White';
+        playerColor = computerSide == goban.BLACK ? 'Black' : 'White';
+      }*/
+      document.getElementById('stats').innerHTML = (scoreLead > 0 ? (katagoColor + ' leads by ') : (playerColor + ' leads by ')) + Math.abs(scoreLead) + ' points';
+      let bestMove = 21 * (row_19+1) + (col_19+1);
+      if (!goban.play(bestMove, computerSide, false)) {
+        if (move != topMoves.length-1) continue;
+        alert('Pass');
+        goban.pass();
+      } goban.refresh(); break;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function eval() { /* Estimate score */
+  /*document.getElementById('stats').innerHTML = 'KataNet is estimating score...';
+  computerSide = goban.side();
+  const bin_inputs = inputTensor();
+  try {
+    tf.setBackend("cpu");
+    const model = await tf.loadGraphModel("./model/model.json");
+    const results = await model.executeAsync({
+        "swa_model/bin_inputs": tf.tensor(bin_inputs, [batches, inputBufferLength, inputBufferChannels], 'float32'),
+        "swa_model/global_inputs": tf.tensor(global_inputs, [batches, inputGlobalBufferChannels], 'float32')
+    });
+    let scores = results[2];
+    let flatScores = scores.dataSync(2);
+    let scoreLead = (flatScores[2]*20).toFixed(2);
+    let katagoColor = computerSide == goban.BLACK ? 'Black' : 'White';
+    let playerColor = (3-computerSide) == goban.BLACK ? 'Black' : 'White';
+    document.getElementById('stats').innerHTML = (scoreLead > 0 ? (katagoColor + ' leads by ') : (playerColor + ' leads by ')) + Math.abs(scoreLead) + ' points';
+  } catch (e) {
+    console.log(e);
+  }*/
+}
