55cb09507cb094cd29ba2db3e3b43be40d937665
Code Monkey King <freesoft.for.people@gmail.com>
2024-10-07 23:02:10 +0300

    fixed passing

--- js/model.js
+++ js/model.js
@@ -139,24 +139,19 @@ async function play(button) { /* Play best move */
     let scores = level ? results[2] : results[1];
     let flatScores = scores.dataSync();
     let copyPolicy = JSON.parse(JSON.stringify(flatPolicyArray));
-    let topPolicies = copyPolicy.sort((a, b) => b - a).slice(0, 5);
-    let topMoves = [];
-    for (let move of topPolicies) topMoves.push(flatPolicyArray.indexOf(move));
-    for (let move = 0; move < topMoves.length; move++) {
-      let best_19 = topMoves[move];
-      let row_19 = Math.floor(best_19 / 19);
-      let col_19 = best_19 % 19;
-      let scoreLead = (flatScores[2]*20).toFixed(2);
-      let katagoColor = computerSide == goban.BLACK ? 'Black' : 'White';
-      let playerColor = (3-computerSide) == goban.BLACK ? 'Black' : 'White';
-      document.getElementById('stats').innerHTML = (scoreLead > 0 ? (katagoColor + ' leads by ') : (playerColor + ' leads by ')) + Math.abs(scoreLead) + ' points';
-      let bestMove = 21 * (row_19+1) + (col_19+1);
-      if (!goban.play(bestMove, computerSide, false)) {
-        if (move != topMoves.length-1) continue;
-        alert('Pass');
-        goban.pass();
-      } goban.refresh(); break;
-    }
+    let topPolicies = copyPolicy.sort((a, b) => b - a).slice(0, 1);
+    let best_19 = flatPolicyArray.indexOf(topPolicies[0]);
+    let row_19 = Math.floor(best_19 / 19);
+    let col_19 = best_19 % 19;
+    let scoreLead = (flatScores[2]*20).toFixed(2);
+    let katagoColor = computerSide == goban.BLACK ? 'Black' : 'White';
+    let playerColor = (3-computerSide) == goban.BLACK ? 'Black' : 'White';
+    document.getElementById('stats').innerHTML = (scoreLead > 0 ? (katagoColor + ' leads by ') : (playerColor + ' leads by ')) + Math.abs(scoreLead) + ' points';
+    let bestMove = 21 * (row_19+1) + (col_19+1);
+    if (!goban.play(bestMove, computerSide, false)) {
+      alert('Pass');
+      goban.pass();
+    } goban.refresh();
   } catch (e) {
     console.log(e);
   }
